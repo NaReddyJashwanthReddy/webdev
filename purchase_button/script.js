1551dbbec26ca2dc94c62cc1c59abbb63a128344
NaReddyJashwanthReddy <tangwulinggd@gmail.com>
2026-06-02 17:09:13 +0530

    purchase button project

---
+++ purchase_button/script.js
@@ -0,0 +1,5 @@
+error_id=document.getElementById("error");
+
+function errorMsg(){
+    error_id.innerHTML = "Sorry, this item is out of stock.";
+}
---
+++ smple_test/script.js
@@ -0,0 +1,9 @@
+
+
+function add3points(points){
+    return points += 3;
+}
+
+function dec1point(points){
+    return points -= 1;
+}
